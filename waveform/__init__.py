afb7d25d2eea8a8786dbaf93ea33da27fc9721ce
Philipp Cochems <philipp.cochems@thermofisher.com>
2019-11-14 21:30:03 +0100

    filled in some functions

--- waveform/__init__.py
+++ waveform/__init__.py
@@ -5,5 +5,28 @@ class waveform:
     t0 = 0
     data = np.array([])
 
-    def trial(self):
-        return 0
+    def __init__(self, dt=1, t0=0, data=np.array([])):
+        '''create a waveform'''
+        self.dt   = dt
+        self.t0   = t0
+        self.data = data
+
+    def rms(self):
+        '''returns the root mean square (RMS) of all values'''
+        return np.sqrt(np.mean(self.data**2))
+
+    def mean(self):
+        '''returns the mean value of the values'''
+        return np.mean(self.data)
+    
+    def ppm(self):
+        '''returns the all values in ppm with the mean value of the values as reference'''
+        return (self.data - np.mean(self.data)) / np.mean(self.data) * 1e6
+
+    def std(self):
+        '''returns the standard deviation of the values'''
+        return np.std(self.data, ddof=1)
+    
+
+
+    
